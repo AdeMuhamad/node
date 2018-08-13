09fd15dfe0561bf20184ce938ea42b57e733d0fb
Your Name <you@example.com>
2018-08-13 09:58:57 +0700

    example auth with local-auth

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://login:login@127.0.0.1:27017/login' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
+
