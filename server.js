e39194eea701d4dd7fc187b8450fb30ddb34dba5
ade muhammad <ade.muhammad@bhinneka.com>
2018-08-20 14:18:07 +0700

    delet node module

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-// server.js
+// server.js 
+// load the things we need
+var express = require('express');
+var app = express();
 
-// set up ======================================================================
-// get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
 
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+// set the view engine to ejs
+app.set('view engine', 'ejs')
 
-var configDB = require('./config/database.js');
 
-// configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
+// use res.render to load up an ejs view file
 
-require('./config/passport.js')(passport); // pass passport for configuration
 
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+// index page
+app.get('/', function(req, res) {
+    var drinks = [
+       { name: 'Bloody Mary', drunkness: 3 },
+       { name: 'Martini', drunkness: 5 },
+       { name: 'Scotch', drunkness: 10}
+    ];
+    
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+    res.render('pages/index', {
+       drinks: drinks,
+       tagline: tagline
 
-// required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+    });
 
-// routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+});
 
-// launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+// about page
+app.get('/about', function(req, res) {
+
+    res.render('pages/about');
+
+});
+
+
+
+app.listen(8080);
+console.log('8080 is the magic port');
 
